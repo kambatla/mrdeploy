a6a8a0725a82a3aff9046257f858ec8342dd781f
Karthik Kambatla <kasha@cloudera.com>
2013-02-02 15:08:01 -0800

    Better hadoop confs

--- src/hadoop1-template/mr1-setup-conf.sh
+++ src/hadoop1-template/mr1-setup-conf.sh
@@ -1,39 +1,24 @@
-#!/bin/bash
+#!/bin/sh
 
 #usage mr1-setup-conf.sh <master> <base_port> <java_home> <HDFS_BASE_DIR>
 
-MASTER=$1
-HADOOP_BASE_PORT=$2
-JAVA_HOME=$3
-HDFS_BASE_DIR=$4
+. ./setup-env.sh
 
-echo "Master: $MASTER"
-echo "Base port: $HADOOP_BASE_PORT"
-echo "Base dir: $HDFS_BASE_DIR"
-
-#####################################################################################################
-
-# DFS WebUI 23070
-# JT WebUI 23030
-
-NAMENODE=$MASTER
-JOB_TRACKER=$MASTER
-
-DFS_NAME_DIR=$HDFS_BASE_DIR/name
-DFS_DATA_DIR=$HDFS_BASE_DIR/data
-MAPRED_LOCAL_DIR=$HDFS_BASE_DIR/mrlocal
-
+## Port configuration
+# DFS
 NAMENODE_PORT=$(($HADOOP_BASE_PORT + 10))
 DFS_DATANODE_PORT=$(($HADOOP_BASE_PORT + 20))
 DFS_DATANODE_IPC_PORT=$(($HADOOP_BASE_PORT + 21))
 DFS_HTTP_PORT=$(($HADOOP_BASE_PORT + 70))
 
+# MR1
 JOB_TRACKER_PORT=$(($HADOOP_BASE_PORT + 11))
 JOB_TRACKER_HTTP_PORT=$((HADOOP_BASE_PORT + 30))
 
-XML_CONFIGS='core-site.xml hdfs-site.xml mapred-site.xml'
+XML_CONFIGS='core-site.xml hdfs-site.xml mapred-site.xml hadoop-env.sh'
 
 for FILE in $XML_CONFIGS; do
+  sed -i -e "s|@JAVA_HOME@|$CLUSTER_JAVA_HOME|" $FILE
   sed -i -e "s|@NAMENODE@|$NAMENODE|" $FILE
   sed -i -e "s|@NAMENODE_PORT@|$NAMENODE_PORT|" $FILE
   sed -i -e "s|@DFS_DATANODE_PORT@|$DFS_DATANODE_PORT|" $FILE
@@ -42,11 +27,20 @@ for FILE in $XML_CONFIGS; do
   sed -i -e "s|@JOB_TRACKER@|$JOB_TRACKER|" $FILE
   sed -i -e "s|@JOB_TRACKER_PORT@|$JOB_TRACKER_PORT|" $FILE
   sed -i -e "s|@JOB_TRACKER_HTTP_PORT@|$JOB_TRACKER_HTTP_PORT|" $FILE
+
+  sed -i -e "s|@HADOOP_TMP_DIR@|$HADOOP_TMP_DIR|" $FILE
+  sed -i -e "s|@HADOOP_LOG_DIR@|$HADOOP_LOG_DIR|" $FILE 
   sed -i -e "s|@DFS_DATA_DIR@|$DFS_DATA_DIR|" $FILE
   sed -i -e "s|@DFS_NAME_DIR@|$DFS_NAME_DIR|" $FILE
   sed -i -e "s|@MAPRED_LOCAL_DIR@|$MAPRED_LOCAL_DIR|" $FILE
-done
 
-sed -i -e "s|@JAVA_HOME@|$JAVA_HOME|" 'hadoop-env.sh'
+  sed -i -e "s|@TT_MAP_TASKS_MAX@|$TT_MAP_TASKS_MAX|" $FILE
+  sed -i -e "s|@TT_REDUCE_TASKS_MAX@|$TT_REDUCE_TASKS_MAX|" $FILE
+  sed -i -e "s|@TT_HTTP_THREADS@|$TT_HTTP_THREADS|" $FILE
+  sed -i -e "s|@REDUCE_PARALLEL_COPIES@|$REDUCE_PARALLEL_COPIES|" $FILE
+  sed -i -e "s|@IO_SORT_MB@|$IO_SORT_MB|" $FILE
+  sed -i -e "s|@IO_SORT_RECORD_PERCENT@|$IO_SORT_RECORD_PERCENT|" $FILE
+  sed -i -e "s|@MAPRED_CHILD_JAVA_OPTS@|$MAPRED_CHILD_JAVA_OPTS|" $FILE
+done
 
 echo $MASTER > masters
---
+++ src/hadoop1-template/setup-env.sh
@@ -0,0 +1,35 @@
+#!/bin/sh
+
+### Setup the following variables to configure cluster MR1/MR2 ###
+
+# Topology
+MASTER=localhost
+NAMENODE=$MASTER
+JOB_TRACKER=$MASTER
+RESOURCE_MANAGER=$MASTER
+SLAVES_FILE=slaves
+
+# Java
+CLUSTER_JAVA_HOME=/home/kasha/toolchain/sun-jdk-64bit-1.6.0.31
+
+# Ports
+HADOOP_BASE_PORT=23000 # DFS: +70, JT: +30
+
+# Directories
+HADOOP_TMP_DIR=/tmp/
+HADOOP_LOG_DIR=/home/kasha/install/hadoop-logs
+
+HDFS_BASE_DIR=/home/kasha/install/hdfs
+DFS_NAME_DIR=$HDFS_BASE_DIR/name
+DFS_DATA_DIR=$HDFS_BASE_DIR/data
+MAPRED_LOCAL_DIR=$HDFS_BASE_DIR/mrlocal
+
+# MR1 confs
+TT_MAP_TASKS_MAX=4
+TT_REDUCE_TASKS_MAX=2
+MAPRED_CHILD_JAVA_OPTS="-Xmx 1024m"
+IO_SORT_MB=320
+IO_SORT_RECORD_PERCENT=0.17
+TT_HTTP_THREADS=8
+REDUCE_PARALLEL_COPIES=4
+
